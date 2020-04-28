2730e3585ecb95eef986086c67fae3d32c2cd6a6
syed mohammad hamza <syedmohammadhamza@syeds-MacBook-Pro.local>
2020-04-28 19:58:18 +0530

    First commit

---
+++ Recr.py
@@ -0,0 +1,28 @@
+
+    
+#def recr_func(i):
+  #  if i == 1:
+  #      return 1
+#
+  #  else:
+    #    return recr_func( i - 1)
+#
+        
+
+        
+#i = 100
+#print(recr_func(i#))
+
+
+def printrecr(test):
+    if(test == 2 ):  
+        return 2
+
+    else:
+
+        print 
+        printrecr(test - 1)
+        return
+
+test = 100
+printrecr(test)   
