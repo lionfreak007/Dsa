87f16baf21ea66d8f269b99e8db45714432d0c84
syed mohammad hamza <syedmohammadhamza@syeds-MacBook-Pro.local>
2020-05-02 14:05:46 +0530

    New commit

--- Quick_sort1.2.py
+++ Quick_sort1.2.py
@@ -1,29 +1,34 @@
 def Partition(arr, low, high):
     i = (low - 1)
-    # 10, 4 ,7 , 8, 1, 2
+
     pivot = arr[high]
     print(low, "  ",  high)
-    for j in range(low, high-1):
-        # pivot  - 2
+
+    for j in range(low, high):
+
         if arr[j] < pivot:
             arr[i + 1], arr[j] = arr[j], arr[i + 1]
+
             i = i + 1
 
     arr[i+1], arr[high] = arr[high], arr[i+1]
-    return (i+1)
+    print(arr)
+
+    return (i + 1)
 
 
 def quickSort(arr, low, high):
-    if low == high:
+    if low >= high:
         return
     p = Partition(arr, low, high)
 
     quickSort(arr, low, p-1)
-    quickSort(arr, p-1, high)
+    quickSort(arr, p+1, high)
+
 
 arr = [10, 5, 3, 7, 1, 9, 4]
 n = len(arr)
 quickSort(arr, 0, n-1)
 print("Sorted array is:")
 for i in range(n):
-    print("%d" % arr[i]),
+    print("%d" % arr[i])
